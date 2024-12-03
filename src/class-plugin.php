a66d7ba059a9280c2dbd9240e0a3459df2f511d3
Felipe Lavín <felipe@yukei.net>
2024-12-03 12:10:08 -0300

    filter urls on srcset attributes

--- src/class-plugin.php
+++ src/class-plugin.php
@@ -53,6 +53,7 @@ class Plugin {
 		$this->attachment_upload_task = new Attachment_Upload_Task();
 		add_filter( 'wp_generate_attachment_metadata', array( $this, 'upload_attachment_files' ), 10, 2 );
 		add_filter( 'wp_get_attachment_url', array( $this, 'filter_attachment_url' ), 10, 2 );
+		add_filter( 'wp_calculate_image_srcset', array( $this, 'filter_srcset_urls' ), 10, 5 );
 		add_action( 'add_attachment', array( $this, 'upload_attachment' ), 10, 1 );
 		if ( defined( 'WP_CLI' ) && WP_CLI ) {
 			$this->cli = new CLI();
@@ -86,6 +87,23 @@ class Plugin {
 		$in_cdn = (bool) get_post_meta( $attachment_id, static::UPLOADED_META_KEY, true );
 		$cdn_url = str_replace( content_url(), untrailingslashit( getenv( 'BLOOM_BUNNY_PUBLIC_URL' ) ), $url );
 		return $in_cdn ? $cdn_url : $url;
+	}
+
+	/**
+	 * Filter URLs for images on the srcset attribute.
+	 *
+	 * @param array  $sources       Original image sources array.
+	 * @param array  $size_array    Array of requested width and height values.
+	 * @param string $image_src     The original "src" attribute of the image.
+	 * @param array  $image_meta    The image meta data as returned by 'wp_get_attachment_metadata()'.
+	 * @param int    $attachment_id The ID for the attachment.
+	 * @return array Filtered srcset URLs.
+	 */
+	public function filter_srcset_urls( $sources, $size_array, $image_src, $image_meta, $attachment_id ): array {
+		foreach ( $sources as $size => $source ) {
+			$sources[ $size ]['url'] = $this->filter_attachment_url( $source['url'], $attachment_id );
+		}
+		return $sources;
 	}
 
 	/**
